958a4c8b56cbc73776c0e9b5f6195d40ff23921f
mustofyou <89636769+mustofyou@users.noreply.github.com>
2022-01-17 23:30:40 +0300

    add mongoose library

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname+"/date.js")
+const date = require(__dirname+"/date.js");
+const mongoose = require("mongoose);
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+mongoose.connect("mongodb://localhost:27017/fruitdb"); //to connect to the database
 
 var items = ["buy food"];
 let workItems = [];
